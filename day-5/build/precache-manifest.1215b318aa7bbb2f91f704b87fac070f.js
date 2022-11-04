427c3a7413051f602ef08f883b7500c4ada875f1
reds98 <sahid.rojas64@gmail.com>
2022-11-03 20:04:24 -0600

    Added the day 5 learned how to create a nice navbar with only css

---
+++ day-5/build/precache-manifest.1215b318aa7bbb2f91f704b87fac070f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bb8cfcaaba6d23572f1a4af54d0fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51df82e26d07cf3f8ef8",
+    "url": "/static/css/main.ff5beaa2.chunk.css"
+  },
+  {
+    "revision": "f1090d6d3220d924b4f2",
+    "url": "/static/js/2.a82e7a49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a82e7a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51df82e26d07cf3f8ef8",
+    "url": "/static/js/main.79996f32.chunk.js"
+  },
+  {
+    "revision": "e924442015caaa7f8ca4",
+    "url": "/static/js/runtime-main.912a30c6.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/static/media/plus.9311af2e.svg"
+  }
+]);
